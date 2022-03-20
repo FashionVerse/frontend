bbc83aa0f3f47e26cffbd4cf48efe6f71869f386
Ashish <kaushik.ashish91@outlook.com>
2022-03-21 00:23:56 +0530

    Code Updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/static/favicon.ico" />
-          {/* <link rel="stylesheet" type="text/css" href="../styles/style.css" /> */}
+          {/* {<link rel="stylesheet" type="text/css" href="../styles/style.css" />} */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -18,6 +18,10 @@ export default class MyDocument extends Document {
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://satvikalai.com/wip/fashion.css"
             rel="stylesheet"
           />
           {
