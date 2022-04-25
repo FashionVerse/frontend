59bd9cf2114a0c02ca449b67afaeb85fc1e76211
Isha <ishasharma@zapbuild.com>
2022-04-25 19:06:23 +0530

    Worked on the design of product detail page.

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -3,7 +3,8 @@ import Viewer from "../../src/components/Viewer";
 import Model from "../../src/components/Model";
 import useSWR from "swr";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-import { Button, Typography,Box } from "@mui/material";
+import { Button, Typography, Box, Container, Grid, Card } from "@mui/material";
+import GridCard, { GridCardProps } from "../../src/components/GridCard";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,6 +15,8 @@ import { ethers } from "ethers";
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import Footer from "../../src/components/Footer";
 import { NextSeo } from "next-seo";
+import Gif from "../../public/product-img.gif";
+import { styled, alpha, useTheme } from "@mui/system";
 
 
 const fetcher = (url) => fetch(url, {method: 'POST'}).then((res) => res.json());
@@ -65,34 +68,69 @@ function AnimatedButton(props)
   const [isAnimating, setIsAnimating] = useState(0);
   return(
     <>
-    <motion.button 
-                type="button" 
-                onClick={() => 
-                  {
-                    setIsAnimating(1)
-                    setCart()
-                  }
-                }
-                style={isAnimating===0? {display:"flex"}:{display:"none"}}
-                class="tw-cursor-pointer tw-flex tw-gap-4 tw-items-center tw-justify-center tw-py-2 tw-px-4 tw-bg-cyan-500 hover:tw-bg-cyan-600 focus:tw-ring-cyan-400 focus:tw-ring-offset-cyan-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 tw-rounded-lg "
-                >
-                  <AccountBalanceWalletIcon /> Add To Bag
-                </motion.button>
-                <motion.button 
-                style={isAnimating===1? {display:"flex"}:{display:"none"}}
-                type="button" class="tw-py-2 tw-px-4 tw-flex tw-justify-center tw-items-center  tw-bg-blue-600 hover:tw-bg-blue-700 focus:tw-ring-blue-500 focus:tw-ring-offset-blue-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2  tw-rounded-lg ">
-                <svg width="20" height="20" fill="currentColor" class="tw-mr-2 tw-animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
-                    </path>
-                </svg>
-                Adding
-              </motion.button>
-              <motion.button 
-                style={isAnimating===2? {display:"flex"}:{display:"none"}}
-                type="button" class="tw-py-2 tw-px-4 tw-gap-2 tw-flex tw-justify-center tw-items-center  tw-bg-green-600 hover:tw-bg-green-700 focus:tw-ring-green-500 focus:tw-ring-offset-green-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2  tw-rounded-lg ">
-                <DoneAllIcon />{' '}
-                Added To Bag
-              </motion.button>
+      {/* <motion.button 
+        type="button" 
+        onClick={() => 
+          {
+            setIsAnimating(1)
+            setCart()
+          }
+        }
+        style={isAnimating===0? {display:"flex"}:{display:"none"}}
+        class="tw-cursor-pointer tw-flex tw-gap-4 tw-items-center tw-justify-center tw-py-2 tw-px-4 tw-bg-cyan-500 hover:tw-bg-cyan-600 focus:tw-ring-cyan-400 focus:tw-ring-offset-cyan-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2 tw-rounded-lg "
+        >
+          <AccountBalanceWalletIcon /> Add To Bag lllll
+        </motion.button> */}
+        
+        <Button 
+          variant="contained" 
+          size="large" 
+          onClick={() => 
+          {
+            setIsAnimating(1)
+            setCart()
+          }}
+          style={isAnimating===0? {display:"flex"}:{display:"none"}}
+          sx={{color: "#fff", width: 1}}
+        >
+          <AccountBalanceWalletIcon /> Add To Bag
+        </Button> 
+        <Button 
+          variant="contained" 
+          size="large" 
+          style={isAnimating===1? {display:"flex"}:{display:"none"}}
+          sx={{color: "#fff", width: 1}}
+        >
+          <svg width="20" height="20" fill="currentColor" class="tw-mr-2 tw-animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
+              </path>
+          </svg> 
+          Adding
+        </Button>      
+        <Button 
+          variant="contained" 
+          size="large" 
+          style={isAnimating===2? {display:"flex"}:{display:"none"}}
+          sx={{color: "#fff", width: 1}}
+        >
+          <DoneAllIcon />{' '}
+          Added To Bag
+        </Button>
+        {/* <motion.button 
+        style={isAnimating===1? {display:"flex"}:{display:"none"}}
+        type="button" class="tw-py-2 tw-px-4 tw-flex tw-justify-center tw-items-center  tw-bg-blue-600 hover:tw-bg-blue-700 focus:tw-ring-blue-500 focus:tw-ring-offset-blue-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2  tw-rounded-lg ">
+        <svg width="20" height="20" fill="currentColor" class="tw-mr-2 tw-animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
+            </path>
+        </svg>
+        Adding kkkkk
+      </motion.button> */}
+      {/* <motion.button 
+        style={isAnimating===2? {display:"flex"}:{display:"none"}}
+        type="button" class="tw-py-2 tw-px-4 tw-gap-2 tw-flex tw-justify-center tw-items-center  tw-bg-green-600 hover:tw-bg-green-700 focus:tw-ring-green-500 focus:tw-ring-offset-green-200 tw-text-white tw-w-full tw-transition tw-ease-in tw-duration-200 tw-text-center tw-text-base tw-font-semibold tw-shadow-md focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-offset-2  tw-rounded-lg ">
+        <DoneAllIcon />{' '}
+        Added To Bag jjjjjj
+      </motion.button> */}
     </>
   )
 
@@ -136,6 +174,9 @@ export default function Product() {
     </Box>
   );
   console.log("data fetched",data);
+  const {
+    palette: { mode },
+  } = useTheme();
   return (
     <>
     <NextSeo
@@ -172,10 +213,257 @@ export default function Product() {
         cardType: 'summary_large_image',
       }}
     />
-      <Head>
-        <title>Product</title>
-      </Head>
-      <div class="tw-flex tw-flex-wrap md:tw-h-screen tw-p-10 tw-h-full tw-gap-x-10 tw-justify-center tw-items-center ">
+    <Head>
+      <title>Product</title>
+    </Head>
+
+    <Container
+        className="wrapper resource-page common-wrapper"
+        maxWidth={false}
+    >
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="custom-container common-fold1"
+      >
+        <Grid
+          sx={{ marginTop: "20px", marginBottom: "100px" }}
+          container
+          direction="row"
+          spacing={4}
+        >
+          <Grid item lg={6}>
+            <div id="product-container"
+              class="tw-shadow-2xl tw-shadow-white/50"
+              style={{
+                backgroundColor: "rgba(0,0,0,0.05)",
+                backdropFilter: "blur(5px)",
+                borderRadius: "20px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: "20px",
+                  overflow: "hidden",
+                  px: 6, py: 6
+                }}
+              >
+                <Image sx={{borderRadius: "20px"}}  src={Gif} alt="gif"  objectFit="cover" />
+                <Button 
+                  variant="outlined"
+                  color={mode === "dark" ? "primary" : "secondary"}
+                  sx={{
+                    alignSelf: "flex-end",
+                    marginTop: "20px"
+                  }}
+                >
+                  Open in 3d
+                </Button>
+              </Box>
+            </div>
+          </Grid>
+          <Grid item lg={6}>
+            <Box
+              sx={{
+                padding: "0 20px 20px;"
+              }}
+            >
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                component="div"
+                sx={{
+                  marginBottom: "10px"
+                }}
+              >
+                {data.nft.metadata.description}
+              </Typography>
+              <Typography
+                variant="h1"
+                component="h1"
+                color="primary"
+                sx={{
+                  fontSize: 'h3.fontSize',
+                  textAlign: 'left',
+                  fontWeight: 700,
+                  marginBottom: "20px"
+                }}
+              >
+                {data.nft.metadata.name}
+              </Typography>
+
+              <Link href={"/brands/"+data.brand.url} className="!tw-cursor-pointer">
+                <motion.div
+                  // className="drops_hover_cursor"
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ ease: "easeOut", delay: 0.1 }}
+                  whileHover={{ scale: 1.01 }}
+                  whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
+                >
+                  <div className="tw-flex tw-flex-col md:tw-flex-row tw-gap-4 tw-items-center tw-pr-5">
+                    <Image src={data.brand.avatarSrc} width="40" height="40" className="tw-rounded-full"/>
+                    <Typography
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                        className="tw-font-light"
+                      >
+                        {data.brand.title}
+                    </Typography>
+                  </div>
+                </motion.div>
+              </Link>
+
+              <Link href={"/collections/"+data.collection._id} className="!tw-cursor-pointer">
+                <motion.div
+                  // className="drops_hover_cursor"
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ ease: "easeOut", delay: 0.1 }}
+                  whileHover={{ scale: 1.01 }}
+                  whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
+                >
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    component="div"
+                    className="tw-font-light"
+                    sx={{ marginTop: "10px" }}
+                  >
+                    <span className="tw-font-semibold">{data.collection.title}</span>
+                  </Typography>
+                </motion.div>
+              </Link>
+
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                component="div"
+                className="tw-font-light"
+                sx={{ marginBottom: "10px" }}
+              >
+                <span className="tw-font-semibold">Rarity:</span> 
+                <div className="tw-animate-gradient-x tw-font-bold tw-bg-clip-text tw-text-transparent tw-bg-gradient-to-l tw-from-rose-400 tw-via-fuchsia-500 tw-to-indigo-500">Ultra Rare</div>
+              </Typography>
+
+              <div>
+                {data.available != 0 ? (
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    component="div"
+                    className="tw-font-semibold"
+                    sx={{ marginBottom: "10px", fontWeight: "600" }}
+                  >
+                    Total Available:{" "}
+                    <div className="tw-font-semibold pl-2">
+                      {data.available}
+                    </div>
+                  </Typography>
+                ) : (
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom
+                    component="div"
+                    className="tw-font-semibold tw-text-red-500"
+                  >
+                    Currently Unavailable
+                  </Typography>
+                )}
+              </div>
+
+              <Box sx={{marginBottom: "30px"}}>
+                <Typography
+                  variant="h2"
+                  gutterBottom
+                  component="div"
+                  className="tw-font-medium"
+                  sx={{
+                    fontSize: 'h6.fontSize',
+                  }}
+                >
+                  Current Price:
+                </Typography>
+                <Typography
+                  variant="h3"
+                  gutterBottom
+                  component="div"
+                  className="tw-font-bold"
+                  sx={{
+                    fontSize: 'h4.fontSize',
+                    fontWeight: 700
+                  }}
+                >
+                  <img
+                    alt="ETH"
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      objectFit: "contain",
+                    }}
+                    src="https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg"
+                    size="24"
+                  />{" "}
+                  {data.price} ETH
+                </Typography>
+              </Box>
+
+              <motion.div
+                // className="drops_hover_cursor"
+                style={{
+                  cursor: "pointer",
+                }}
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ ease: "easeOut", delay: 0.1 }}
+                whileHover={{ scale: 1.015 }}
+                whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
+              >
+                {data.available != 0 ? (
+                  <>
+                    <AnimatedButton id={data._id}/>
+                  </>
+                ) : (                  
+                  <Button 
+                    disabled
+                    variant="contained" 
+                    size="large"
+                    sx={{color: "#fff", width: 1}}
+                  >
+                    <AccountBalanceWalletIcon /> Add To Bag
+                  </Button>
+                )}
+              </motion.div>
+            </Box>           
+          </Grid>
+        </Grid>
+      </Grid>
+      <Footer />
+    </Container>
+
+
+
+
+
+
+
+
+
+      {/* <div class="tw-flex tw-flex-wrap md:tw-h-screen tw-p-10 tw-h-full tw-gap-x-10 tw-justify-center tw-items-center ">
         <div id="product-container"
           class="tw-mb-4 tw-px-4 md:tw-w-1/2 tw-w-full tw-shadow-2xl tw-h-[65vh] lg:tw-h-full tw-shadow-white/50"
           style={{
@@ -184,13 +472,13 @@ export default function Product() {
             borderRadius: "20px",
           }}
         >
-          {/* <Image
+          <Image
             src="/hero-circle.svg"
             alt="..."
             layout="fill"
             priority
             loading="eager"
-          /> */}
+          />
           <Viewer width="100%" height="100%" imgLink={data.nft.metadata.image} isProduct="true">
             <Model
               link={data.nft.metadata.animation_url + "?filename=model.glb"}
@@ -304,7 +592,7 @@ export default function Product() {
                 </Typography>
               )}
             </div>
-            {/* <br/> */}
+            
             <div>
               <Typography
                 variant="h6"
@@ -334,7 +622,7 @@ export default function Product() {
               </Typography>
             </div>
             <motion.div
-              // className="drops_hover_cursor"
+//className="drops_hover_cursor"
               style={{
                 cursor: "pointer",
               }}
@@ -359,13 +647,12 @@ export default function Product() {
                 </Button>
               )}
             </motion.div>
-            {/* <br/> */}
           </div>
         </div>
       </div>
       <div className="tw-pt-[4rem] tw-pb-[4rem]">
         <Footer />
-      </div>
+      </div> */}
     </>
   );
 }
