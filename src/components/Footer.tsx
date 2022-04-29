f9c036cccd504bd634a6ca05880d85eed0d0b5ab
shreyassai123 <shreyassai2@gmail.com>
2022-04-29 08:59:15 +0530

    Changed suppport

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -150,5 +150,7 @@ const ABOUT_LINKS = [
 
 const SUPPORT_LINKS = [
   { label: "Contact us", href: "contact", id: "lpokiu" },
+  { label: "Suggestion Form", href: "https://docs.google.com/forms/d/e/1FAIpQLSerQnqtUGZSnUEHFgbd2Unmdv0_0tcnIS1YVIJjp3D5BzKfjg/viewform?usp=sf_link", id: "abcd" },
+  { label: "Questions Form", href: "https://docs.google.com/forms/d/e/1FAIpQLSdFo9dZhe-19nwXYOlQbwiZzr_jk9R7I85SYBje33PCeQ6yeA/viewform?usp=sf_link", id: "xyz" },
   // { label: "Bug bounty", href: "bug-bounty", id: "qwerty" },
 ];
