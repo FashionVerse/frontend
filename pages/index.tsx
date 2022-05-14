fdb8269e30903b7ee2b12beaee5208d2a674732d
shreyassai123 <shreyassai2@gmail.com>
2022-05-14 07:39:10 +0530

    Added SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -238,35 +238,10 @@ export default function Index() {
     <>
       <NextSeo
         title="The FashionVerse"
-        description="This example uses more of the available config options."
-        canonical="https://www.canonical.ie/"
-        openGraph={{
-          url: "https://www.url.ie/a",
-          title: "Open Graph Title",
-          description: "Open Graph Description",
-          images: [
-            {
-              url: "https://www.example.ie/og-image-01.jpg",
-              width: 800,
-              height: 600,
-              alt: "Og Image Alt",
-              type: "image/jpeg",
-            },
-            {
-              url: "https://www.example.ie/og-image-02.jpg",
-              width: 900,
-              height: 800,
-              alt: "Og Image Alt Second",
-              type: "image/jpeg",
-            },
-            { url: "https://www.example.ie/og-image-03.jpg" },
-            { url: "https://www.example.ie/og-image-04.jpg" },
-          ],
-          site_name: "SiteName",
-        }}
+        description="The FashionVerse, Inc. is an NFT marketplace for 3D digital clothing that enables the user to build an metaverse compatible digital closet."
+        canonical="https://www.thefashionverse.io/"
         twitter={{
-          handle: "@handle",
-          site: "@site",
+          handle: "@FashionVerseInc",
           cardType: "summary_large_image",
         }}
       />
